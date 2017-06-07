510b9ed312facfe4019f5d75e12391547525cec5
Daniel Halliday <daniel.j.halliday@gmail.com>
2017-06-07 15:41:19 +0100

    initial commit

---
+++ ReadMe.txt
@@ -0,0 +1,18 @@
+HR analytics data - a flask, d3, dc, crossfilter project
+
+Data sourced from: https://www.kaggle.com/ludobenistant/hr-analytics
+
+
+
+Kaggle challenge:(not relevant for the stream 2 project at Code Institute but nice to have some ideas about possible insights)
+Why are our best and most experienced employees leaving prematurely? Have fun with this database and try to predict which valuable employees will leave next. Fields in the dataset include:
+
+Last evaluation
+Number of projects
+Average monthly hours
+Time spent at the company
+Whether they have had a work accident
+Whether they have had a promotion in the last 5 years
+Department
+Salary
+Whether the employee has left
---
+++ hrdata.py
@@ -0,0 +1,53 @@
+from flask import Flask
+from flask import render_template
+from pymongo import MongoClient
+import json
+ 
+app = Flask(__name__)
+ 
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
+DBS_NAME = 'hrData'
+COLLECTION_NAME = 'projects'
+ 
+ 
+@app.route("/")
+def index():
+    """
+    A Flask view to serve the main dashboard page.
+    """
+    return render_template("index.html")
+ 
+ 
+@app.route("/hrData/projects")
+def hrData_projects():
+    """
+    A Flask view to serve the project data from
+    MongoDB in JSON format.
+    """
+ 
+    # A constant that defines the record fields that we wish to retrieve.
+    FIELDS = {
+        '_id': False, 'satisfaction_level': True, 'last_evaluation': True,
+        'number_project': True, 'average_monthly_hours': True,
+        'time_spend_company': True, 'work_accident': True,
+        'left': True,
+        'promotion_last_5years': True,
+        'sales': True,
+        'salary': True
+    }
+ 
+    # Open a connection to MongoDB using a with statement such that the
+    # connection will be closed as soon as we exit the with statement
+    with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
+        # Define which collection we wish to access
+        collection = conn[DBS_NAME][COLLECTION_NAME]
+        # Retrieve a result set only with the fields defined in FIELDS
+        # and limit the the results to 55000
+        projects = collection.find(projection=FIELDS, limit=55000)
+        # Convert projects to a list in a JSON object and return the JSON data
+        return json.dumps(list(projects))
+ 
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
